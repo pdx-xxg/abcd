424613c355b991e3d4c505408665e26de0e6920f
luan <zhuli200597@qq.com>
2025-12-19 22:39:18 +0800

    feat: 新增orange事务部和学习部vue页面

---
+++ src/views/orange事务部.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="page-container">
+<div class="img-container">
+        <img src="../assets/事务部.png">
+    </div>
+    <br>
+    <br>
+    <br>
+    <div class="intro-container">
+        <div class="intro-img">
+            <img src="../assets/事务部2.jpg">
+        </div>
+        <div class="intro-text">
+            <h1>
+                事务部简介
+            </h1>
+            <p>
+                事务部是一个非常重要的部门，其职责涵盖了各个方面，包括活动记录、人事管理和财务管理等。这些职责使得事务部成为了组织中的核心力量，对于保证组织的顺利运转和决策的准确性起到了至关重要的作用。
+            </p>
+            <p>
+                同时，事务部还要负责沟通和协调各个部门，确保信息的及时传递和工作的顺利协调。这样的职责使得事务部成为了工作室的参谋和副手，为组织的决策和发展提供了重要的支持和帮助。
+            </p>
+        </div>
+
+    </div>
+    <br>
+    <br>
+    <br>
+  </div>
+    
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    html, body {
+    margin: 0;
+    padding: 0;
+    width: 100%;
+}
+.img-container img {
+    width: 100%;
+    height: auto;
+}
+
+.intro-container {
+    display: flex;
+    margin: 0 auto;
+    border: 1px solid #FCFCFC;
+    border-radius: 8px;
+    max-width: 1000px;
+    box-shadow: 0 2px 30px rgba(0, 0, 0, 0.1);
+
+
+}
+
+.intro-img img {
+    width: 400px;
+}
+
+.intro-text {
+    padding: 0 20px;
+}
+
+.intro-text h1 {
+    font-weight: 400;
+}
+
+.intro-text p {
+    text-indent: 2em;
+    font-size: 18px;
+    font-weight: 250;
+    line-height: 25px;
+
+}
+</style>
---
+++ src/views/orange学习部.vue
@@ -0,0 +1,81 @@
+<template>
+    <div class="page-container">
+<div class="img-container">
+        <img src="../assets/学习部.png">
+    </div>
+    <br>
+    <br>
+    <br>
+    <div class="intro-container">
+        <div class="intro-img">
+            <img src="../assets/学习部2.jpg">
+        </div>
+        <div class="intro-text">
+            <h1>
+                学习部简介
+            </h1>
+            <p>
+                学习部是橙果工作室负责学习相关事务的核心部门。在这个部门中，我们管理丰富多样的学习资源，同时也策划并推动各类学习活动。我们的使命还包括监督工作室成员的学习进展，确保他们能够高效地管理自己的学习。
+            </p>
+            <p>
+                在学习部，我们致力于营造一个积极的学习氛围，激发成员的学习热情。通过不断创新和改进，我们努力推动工作室成员在各自领域的不断成长和进步。
+            </p>
+        </div>
+
+    </div>
+    <br>
+    <br>
+    <br>
+    </div>
+   
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    .page-container {
+  width: 100vw; /* 宽度占满浏览器可视区域 */
+  min-height: 100vh; /* 高度至少占满可视区域（内容多的话会自动延伸） */
+  box-sizing: border-box; /* 避免padding/margin撑大盒子 */
+}
+.img-container img {
+    width: 100%;
+    height: auto;
+}
+
+.intro-container {
+    display: flex;
+
+    margin: 0 auto;
+    border: 1px solid #FCFCFC;
+    border-radius: 8px;
+    max-width: 1000px;
+    box-shadow: 0 2px 30px rgba(0, 0, 0, 0.1);
+
+}
+
+.intro-img img {
+    width: 400px;
+    height: 300px;
+}
+
+.intro-text {
+    padding: 0 20px;
+}
+
+.intro-text h1 {
+    font-weight: 400;
+}
+
+.intro-text p {
+    text-indent: 2em;
+    font-size: 18px;
+    font-weight: 250;
+    line-height: 25px;
+
+}
+</style>
