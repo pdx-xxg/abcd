4a3bcbe6dd5345b7b6eaa5c7a87914856978909a
zhu-yueyue12 <3757399601@qq.com>
2025-12-20 13:24:27 +0800

    提交说明：新增网站页脚

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
-  <router-view />
+
+  <router-view/>
 </template>
 
-<style scoped>
+<style>
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
 
+  color: #2c3e50;
+}
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,5 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import App from './App.vue'
 import router from './router'
 
-import App from './App.vue'
-
-const app = createApp(App)
-
-app.use(router)
-app.use(createPinia())
-
-app.mount('#app')
+createApp(App).use(router).mount('#app')
